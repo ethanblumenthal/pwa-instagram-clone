2393409e32e03eb7523d52441ae7519a2228911c
ethanblumenthal <ethan.blumenthal@gmail.com>
2019-10-30 16:15:06 -0500

    Implemented native device features such as camera and file upload.

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,14 @@
 importScripts('/src/js/idb.js');
 importScripts('/src/js/utility.js');
 
-var CACHE_STATIC_NAME = 'static-v24';
-var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+var CACHE_STATIC_NAME = 'static-v40';
+var CACHE_DYNAMIC_NAME = 'dynamic-v3';
 var STATIC_FILES = [
   '/',
   '/index.html',
   '/offline.html',
   '/src/js/app.js',
+  '/src/js/utility.js',
   '/src/js/feed.js',
   '/src/js/idb.js',
   '/src/js/promise.js',
@@ -18,7 +19,7 @@ var STATIC_FILES = [
   '/src/images/main-image.jpg',
   'https://fonts.googleapis.com/css?family=Roboto:400,700',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
-  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
 
 // function trimCache(cacheName, maxItems) {
@@ -34,37 +35,36 @@ var STATIC_FILES = [
 //     })
 // }
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
-    caches.open(CACHE_STATIC_NAME).then(function(cache) {
-      console.log('[Service Worker] Precaching App Shell');
-      cache.addAll(STATIC_FILES);
-    }),
-  );
+    caches.open(CACHE_STATIC_NAME)
+      .then(function (cache) {
+        console.log('[Service Worker] Precaching App Shell');
+        cache.addAll(STATIC_FILES);
+      })
+  )
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
   console.log('[Service Worker] Activating Service Worker ....', event);
   event.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(
-        keyList.map(function(key) {
+    caches.keys()
+      .then(function (keyList) {
+        return Promise.all(keyList.map(function (key) {
           if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
             console.log('[Service Worker] Removing old cache.', key);
             return caches.delete(key);
           }
-        }),
-      );
-    }),
+        }));
+      })
   );
   return self.clients.claim();
 });
 
 function isInArray(string, array) {
   var cachePath;
-  if (string.indexOf(self.origin) === 0) {
-    // request targets domain where we serve the page from (i.e. NOT a CDN)
+  if (string.indexOf(self.origin) === 0) { // request targets domain where we serve the page from (i.e. NOT a CDN)
     console.log('matched ', string);
     cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
   } else {
@@ -73,49 +73,55 @@ function isInArray(string, array) {
   return array.indexOf(cachePath) > -1;
 }
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
+
   var url = 'https://pwagram-99adf.firebaseio.com/posts';
   if (event.request.url.indexOf(url) > -1) {
-    event.respondWith(
-      fetch(event.request).then(function(res) {
+    event.respondWith(fetch(event.request)
+      .then(function (res) {
         var clonedRes = res.clone();
         clearAllData('posts')
-          .then(function() {
+          .then(function () {
             return clonedRes.json();
           })
-          .then(function(data) {
+          .then(function (data) {
             for (var key in data) {
-              writeData('posts', data[key]);
+              writeData('posts', data[key])
             }
           });
         return res;
-      }),
+      })
     );
   } else if (isInArray(event.request.url, STATIC_FILES)) {
-    event.respondWith(caches.match(event.request));
+    event.respondWith(
+      caches.match(event.request)
+    );
   } else {
     event.respondWith(
-      caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else {
-          return fetch(event.request)
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
-                // trimCache(CACHE_DYNAMIC_NAME, 3);
-                cache.put(event.request.url, res.clone());
-                return res;
+      caches.match(event.request)
+        .then(function (response) {
+          if (response) {
+            return response;
+          } else {
+            return fetch(event.request)
+              .then(function (res) {
+                return caches.open(CACHE_DYNAMIC_NAME)
+                  .then(function (cache) {
+                    // trimCache(CACHE_DYNAMIC_NAME, 3);
+                    cache.put(event.request.url, res.clone());
+                    return res;
+                  })
+              })
+              .catch(function (err) {
+                return caches.open(CACHE_STATIC_NAME)
+                  .then(function (cache) {
+                    if (event.request.headers.get('accept').includes('text/html')) {
+                      return cache.match('/offline.html');
+                    }
+                  });
               });
-            })
-            .catch(function(err) {
-              return caches.open(CACHE_STATIC_NAME).then(function(cache) {
-                if (event.request.headers.get('accept').includes('text/html')) {
-                  return cache.match('/offline.html');
-                }
-              });
-            });
-        }
-      }),
+          }
+        })
     );
   }
 });
@@ -181,35 +187,36 @@ self.addEventListener('sync', function(event) {
   if (event.tag === 'sync-new-posts') {
     console.log('[Service Worker] Syncing new Posts');
     event.waitUntil(
-      readAllData('sync-posts').then(function(data) {
-        for (var dt of data) {
-          fetch('https://us-central1-pwagram-99adf.cloudfunctions.net/storePostData', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              Accept: 'application/json',
-            },
-            body: JSON.stringify({
-              id: dt.id,
-              title: dt.title,
-              location: dt.location,
-              image:
-                'https://firebasestorage.googleapis.com/v0/b/pwagram-99adf.appspot.com/o/sf-boat.jpg?alt=media&token=19f4770c-fc8c-4882-92f1-62000ff06f16',
-            }),
-          })
-            .then(function(res) {
-              console.log('Sent data', res);
-              if (res.ok) {
-                res.json().then(function(resData) {
-                  deleteItemFromData('sync-posts', resData.id);
-                });
-              }
+      readAllData('sync-posts')
+        .then(function(data) {
+          for (var dt of data) {
+            var postData = new FormData();
+            postData.append('id', dt.id);
+            postData.append('title', dt.title);
+            postData.append('location', dt.location);
+            postData.append('rawLocationLat', dt.rawLocation.lat);
+            postData.append('rawLocationLng', dt.rawLocation.lng);
+            postData.append('file', dt.picture, dt.id + '.png');
+
+            fetch('https://us-central1-pwagram-99adf.cloudfunctions.net/storePostData', {
+              method: 'POST',
+              body: postData
             })
-            .catch(function(err) {
-              console.log('Error while sending data', err);
-            });
-        }
-      }),
+              .then(function(res) {
+                console.log('Sent data', res);
+                if (res.ok) {
+                  res.json()
+                    .then(function(resData) {
+                      deleteItemFromData('sync-posts', resData.id);
+                    });
+                }
+              })
+              .catch(function(err) {
+                console.log('Error while sending data', err);
+              });
+          }
+
+        })
     );
   }
 });
@@ -226,19 +233,20 @@ self.addEventListener('notificationclick', function(event) {
   } else {
     console.log(action);
     event.waitUntil(
-      clients.matchAll().then(function(clis) {
-        var client = clis.find(function(c) {
-          return c.visibilityState === 'visible';
-        });
+      clients.matchAll()
+        .then(function(clis) {
+          var client = clis.find(function(c) {
+            return c.visibilityState === 'visible';
+          });
 
-        if (client !== undefined) {
-          client.navigate(notification.data.url);
-          client.focus();
-        } else {
-          clients.openWindow(notification.data.url);
-        }
-        notification.close();
-      }),
+          if (client !== undefined) {
+            client.navigate(notification.data.url);
+            client.focus();
+          } else {
+            clients.openWindow(notification.data.url);
+          }
+          notification.close();
+        })
     );
   }
 });
@@ -250,7 +258,7 @@ self.addEventListener('notificationclose', function(event) {
 self.addEventListener('push', function(event) {
   console.log('Push Notification received', event);
 
-  var data = { title: 'New!', content: 'Something new happened!', openUrl: '/' };
+  var data = {title: 'New!', content: 'Something new happened!', openUrl: '/'};
 
   if (event.data) {
     data = JSON.parse(event.data.text());
@@ -261,9 +269,32 @@ self.addEventListener('push', function(event) {
     icon: '/src/images/icons/app-icon-96x96.png',
     badge: '/src/images/icons/app-icon-96x96.png',
     data: {
-      url: data.openUrl,
-    },
+      url: data.openUrl
+    }
   };
 
-  event.waitUntil(self.registration.showNotification(data.title, options));
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
